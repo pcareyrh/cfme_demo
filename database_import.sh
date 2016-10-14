04f3315a39ce6a06091877116061b07d7b03468a
Patrick Carey <pcarey@dhcp-1-89.fab.redhat.com>
2016-10-14 19:33:26 +0100

    updated for vbos

--- database_import.sh
+++ database_import.sh
@@ -1,36 +1,58 @@
 #!/bin/bash
+cd /repo
+echo "Starting download of latest Demo DB. This may take a few minutes"
+#sudo wget --progress=bar:force http://10.9.62.89/dumps/vmdb_production_latest.dump -O /repo/vmdb_production_latest.dump
+sudo wget --progress=bar:force http://10.33.1.49:8000/vmdb_production_latest.dump -O /repo/vmdb_production_latest.dump
 
-#Stop evmserverd from running while we import the demo db.
-if /bin/systemctl -q is-active evmserverd.service
-  then
-    echo "Stopping evmserverd"
-    sudo systemctl stop evmserverd.service
-fi
 
-#Drop the newly created database.
-if /opt/rh/rh-postgresql94/root/usr/bin/dropdb --if-exists vmdb_production
-then
-  echo "Drop initial default database"
+echo "Starting download of Demo DB key"
+sudo wget http://10.9.62.89/dumps/v2_key -O /repo/v2_key
+
+echo "Starting Demo database import."
+echo "Copy Demo DB and Key to host"
+sudo su - root -c "mv -f /repo/v2_key /var/www/miq/vmdb/certs/"
+sudo su - root -c "mv /repo/vmdb_production_latest.dump /var/lib/pgsql"
+
+SERVICE="evmserverd.service"
+
+systemctl -q is-active $SERVICE
+if [ $? -ne 0 ]; then
+  echo "$SERVICE is currently stopped"
 else
-  echo "Drop database failed."
+  echo "$SERVICE was running so attempting stop"
+  systemctl stop $SERVICE
+  echo "$SERVICE stopped."
+
 fi
 
-#Create new database for the demo db import.
-echo "Create and import Demo DB"
-if [[ `sudo su - postgres -c "psql -c \"create database vmdb_production\""` ]]; then
-  echo "Create database complete"
+echo "Dropping initial default database"
+su - postgres -c "dropdb vmdb_production"
+if [ $? -ne 0 ]; then
+  echo "Database drop failed. Exiting."
+  exit $?
 else
-  echo "Create new database failed"
-  exit 1
+  echo "Default database dropped successfully."
 fi
 
-#Import demo database.
-if /opt/rh/rh-postgresql94/root/usr/bin/pg_restore -U root -w -d vmdb_production /var/lib/pgsql/cloudformsDatabase4.1.005.db
-then
-  echo "Import Complete"
+echo "Creating Demo database"
+su - postgres -c "psql -c \"create database vmdb_production\""
+if [ $? -ne 0 ]; then
+  echo "Database create failed. Exiting"
+  exit $?
 else
-  echo "Import failed"
-  exit 1
+  echo "Database created successfully."
 fi
-#  if [ ! $? -eq 1 ]; then
 
+echo "Importing demo database."
+`su - postgres -c "pg_restore -e -U root -w -d vmdb_production vmdb_production_latest.dump"`
+if [ $? -ne 0 ]; then
+  echo "Demo database import failed. Exiting"
+  exit $?
+else
+  echo "Demo database import successful."
+fi
+
+echo "Completed Demo database import"
+echo "Connect to http://$(ifconfig eth0 | awk '/inet / { print $2 }') after the reboot"
+
+reboot 
---
+++ short_post.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+localectl set-keymap us
+timedatectl set-timezone Europe/London
+timedatectl set-ntp true
+
+echo "Subscribing host."
+rm -f /etc/yum.repos.d/*
+subscription-manager repos --disable=*
+subscription-manager repos --enable=rhel-7-server-rpms \
+                --enable=rhel-7-server-optional-rpms \
+                --enable=rhel-7-server-rh-common-rpms \
+                --enable=rhel-7-server-extras-rpms \
+                --enable=cf-me-5.5-for-rhel-7-rpms \
+                --enable=rhel-server-rhscl-7-rpms
+echo "Updating host"
+yum install -y yum-plugin-priorities wget yum-utils vim
+#yum update -y
+
+echo "Commencing Cloudforms initial setup."
+if [ -e /dev/sdb ]; then
+  echo "Setup Virtual Box"
+  appliance_console_cli --host=$(hostname) --region=10 --internal --password="smartvm" --key --force-key --dbdisk=/dev/sdb
+elif [ -e /dev/vdb ]; then
+  echo "Setup LibVirt"
+  appliance_console_cli --host=$(hostname) --region=10 --internal --password="smartvm" --key --force-key --dbdisk=/dev/vdb
+fi
+echo "Completed Cloudforms initial setup."
+
