0ce9b6a2170843470f9ede08bb7f190f3b90ce31
Patrick Carey <pcarey@redhat.com>
2016-11-28 11:55:52 +0000

    updated URL and credentials file

--- database_import.sh
+++ database_import.sh
@@ -2,7 +2,8 @@
 
 # This script is designed to download and import a CF demo database
 # into a new CF appliance.
-DATABASE_URL="10.39.166.29"
+DATABASE_URL="10.9.62.89"
+CREDENTIALS_URL="10.9.62.89"
 
 cd /repo
 echo "Starting download of latest Demo DB. This may take a few minutes"
@@ -16,6 +17,7 @@ fi
 
 echo "Starting download of Demo DB key"
 sudo wget http://$DATABASE_URL/dumps/v2_key -O /repo/v2_key
+sudo wget http://$CREDENTIALS_URL/images/credentials.txt -O /home/vagrant/credentials.txt
 if [ $? -ne 0 ]; then
   echo "Database key download failed. Exiting."
   exit $?
@@ -26,7 +28,6 @@ fi
 
 echo "Copy Demo DB and Key to host"
 sudo su - root -c "mv -f /repo/v2_key /var/www/miq/vmdb/certs/"
-sudo su - root -c "mv /repo/vmdb_production_latest.dump /var/lib/pgsql"
 
 SERVICE="evmserverd.service"
 
@@ -58,7 +59,7 @@ else
 fi
 
 echo "Importing demo database."
-`su - postgres -c "pg_restore -e -U root -w -d vmdb_production vmdb_production_latest.dump"`
+`su - postgres -c "pg_restore -e -U root -w -d vmdb_production /repo/vmdb_production_latest.dump"`
 if [ $? -ne 0 ]; then
   echo "Demo database import failed. Exiting"
   exit $?
@@ -67,6 +68,7 @@ else
 fi
 
 echo "Completed Demo database import"
+sudo su - root -c "rm /repo/vmdb_production_latest.dump"
 echo "Connect to https://$(ifconfig eth0 | awk '/inet / { print $2 }') after the reboot"
 
 reboot 
